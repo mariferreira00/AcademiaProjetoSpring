abf93b1cc385b2cb4656c7a90b5cc32ae3d5975d
Maria Ferreira <81054753+mariferreira00@users.noreply.github.com>
2021-07-20 21:45:52 -0300

    validation

--- src/main/java/academiaJava/projetoSpring/model/Aluno.java
+++ src/main/java/academiaJava/projetoSpring/model/Aluno.java
@@ -31,7 +31,7 @@ public class Aluno {
 	@Length(min = 11, max = 14, message = "O CPF deverá ter entre {min} e {max} caracteres!")
 	private String cpf;	
 	@NotBlank(message = "O campo NOME é obrigatório, por favor, informe um nome!")
-	@Length(min = 3, max = 35, message = "O nome deverá ter no máximo {max} caracteres")
+	@Length(min = 3, max = 35, message = "O nome deverá ter no mínimo {min} e no máximo {max} caracteres")
 	private String nome;
 	@NotBlank(message = "O campo Curso é obrigatório, por favor, informe o nome do curso!")
 	private String nomeCurso;
